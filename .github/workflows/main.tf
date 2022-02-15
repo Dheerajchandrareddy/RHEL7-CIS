ecb7622cb8a92b69bf0246fe1e1a1bf44fa51373
Mark Bolwell <mark.bollyuk@gmail.com>
2022-02-15 17:04:52 +0000

    ssh work on ec2
    Signed-off-by: Mark Bolwell <mark.bollyuk@gmail.com>

--- .github/workflows/main.tf
+++ .github/workflows/main.tf
@@ -60,7 +60,7 @@ resource "aws_security_group" "allow_ssh" {
 resource "aws_instance" "testing_vm" {
     ami                         = var.ami_id
     associate_public_ip_address = true
-    key_name                    = aws_key_pair.key_pair.key_name
+//    key_name                    = aws_key_pair.key_pair.key_name
     instance_type   = var.instance_type
     tags            = var.instance_tags
     vpc_security_group_ids      = [aws_security_group.allow_ssh.id]
