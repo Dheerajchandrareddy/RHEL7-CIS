a36c86c8fe4f089d1f553c0c1a9a0c61d78dd197
Mark Bolwell <mark.bollyuk@gmail.com>
2022-02-15 12:54:58 +0000

    updated terraform settings
    Signed-off-by: Mark Bolwell <mark.bollyuk@gmail.com>

---
+++ .github/workflows/main.tf
@@ -0,0 +1,105 @@
+terraform {
+    required_providers {
+        aws = {
+        source = "hashicorp/aws"
+        }
+    }
+    required_version = ">=0.14.4"
+}
+
+provider "aws" {
+    profile = "default"
+    region  = var.aws_region
+}
+
+// Generate the SSH keypair that we’ll use to configure the EC2 instance.
+// After that, write the private key to a local file and upload the public key to AWS
+
+
+resource "tls_private_key" "key" {
+  algorithm = "RSA"
+}
+
+resource "local_file" "private_key" {
+  filename          = "${path.module}/var.private_key.rsa"
+  sensitive_content = tls_private_key.key.private_key_pem
+  file_permission   = "0400"
+}
+
+
+resource "aws_key_pair" "key_pair" {
+  key_name   = local_file.private_key.filename
+  public_key = tls_private_key.key.public_key_openssh
+}
+
+// Create a security group with access to port 22 and port 80 open to serve HTTP traffic
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+
+resource "aws_security_group" "allow_ssh" {
+  vpc_id = data.aws_vpc.default.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+// instance setup
+
+resource "aws_instance" "testing_vm" {
+    ami                         = var.ami_id
+    associate_public_ip_address = true
+    key_name                    = aws_key_pair.key_pair.key_name
+    instance_type   = var.instance_type
+    tags            = var.instance_tags
+    vpc_security_group_ids      = [aws_security_group.allow_ssh.id]
+
+
+// SSH into instance 
+  connection {
+    
+    # Host name
+    host = self.public_ip
+    # The default username for our AMI
+    user = var.ami_username
+    # Private key for connection
+    private_key = file(pathexpand(var.private_key))
+    # Type of connection
+    type = "ssh"
+  }
+
+  provisioner "file" {
+   source = "s3copy.sh"
+   destination = "${var.ami_user_home}/s3copy.sh"
+ }
+}
+
+// generate inventory file
+resource "local_file" "inventory" {
+  filename = "./hosts.yml"
+  content  = <<EOF
+    # benchmark host
+    all:
+      hosts:
+        var.ami_os:
+          ansible_host: ${aws_instance.testing_vm.public_ip}
+      vars:
+        setup_audit: true
+        run_audit: true
+        system_is_ec2: true
+        .var.ansible_inventory_vars
+    EOF
+}
---
+++ .github/workflows/variables.tf
@@ -0,0 +1,49 @@
+// Taken from the terraform.tfvars
+
+variable "aws_region" {
+    description = "AWS region"
+    type        = string
+}
+
+variable "instance_type" {
+    description = "EC2 Instance Type"
+    type        = string
+}
+    
+variable "instance_tags" {
+    description = "Tags to set for instances"
+    type        = map(string)
+}
+
+variable "private_key" {
+    description = "name of the private key file"
+    default     = "private_key.rsa"
+  
+}
+
+// Taken from the OSname.tfvars
+
+variable "ami_id" {
+    description = "AMI ID"
+    type        = string
+}
+    
+variable "ami_os" {
+    description = "AMI OS Type"
+    type        = string
+}
+
+variable "ami_username" {
+    description = "Username for the ami id"
+    type        = string
+}
+
+variable "ami_user_home" {
+    description = "home directory for the user"
+    type        = string
+}
+
+variable "ansible_inventory_vars" {
+    description = "variables for ansible inventory"
+    type        = map(string)
+}
