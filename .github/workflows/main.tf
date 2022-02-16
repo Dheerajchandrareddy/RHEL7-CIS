c7c3ddba1cc39fa78e8cea90194417d14e9ab3ef
Mark Bolwell <mark.bollyuk@gmail.com>
2022-02-16 08:23:08 +0000

    ssh stuff still

--- .github/workflows/main.tf
+++ .github/workflows/main.tf
@@ -57,6 +57,7 @@ resource "aws_instance" "testing_vm" {
   ami                         = var.ami_id
   associate_public_ip_address = true
 //  key_name                    = aws_key_pair.key_pair.key_name
+  key_name                    = tls_private_key.github_actions.public_key_openssh
   instance_type               = var.instance_type
   tags                        = var.instance_tags
   vpc_security_group_ids      = [aws_security_group.allow_ssh.id]
