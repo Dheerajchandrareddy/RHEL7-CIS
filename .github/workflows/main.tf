0bcdd1a1d1748400deb34ac54ebf388d1b4e7ed3
Mark Bolwell <mark.bollyuk@gmail.com>
2022-02-17 11:48:42 +0000

    tidy up
    Signed-off-by: Mark Bolwell <mark.bollyuk@gmail.com>

--- .github/workflows/main.tf
+++ .github/workflows/main.tf
@@ -3,30 +3,11 @@ provider "aws" {
   region  = var.aws_region
 }
 
-// Generate the SSH keypair that we’ll use to configure the EC2 instance.
-// After that, write the private key to a local file and upload the public key to AWS
-
-//variable "key_name" {
-//  default = "github_actions"
-//}
-
-//resource "tls_private_key" "github_actions" { # Generate key
-//  algorithm = "RSA"
-//  rsa_bits  = 4096
-//}
-
-//resource "aws_key_pair" "key_pair" {
-//  key_name   = var.key_name # Add temp_key to AWS
-//  public_key = tls_private_key.github_actions.public_key_openssh
-//}
-
-
 // Create a security group with access to port 22 and port 80 open to serve HTTP traffic
 
 data "aws_vpc" "default" {
   default = true
 }
-
 
 resource "aws_security_group" "allow_ssh" {
   vpc_id = data.aws_vpc.default.id
@@ -62,24 +43,6 @@ resource "aws_instance" "testing_vm" {
   instance_type               = var.instance_type
   tags                        = var.instance_tags
   vpc_security_group_ids      = [aws_security_group.allow_ssh.id]
-
-  // SSH into instance 
-  //  connection {
-
-  //    # Host name
-  //    host = self.public_ip
-  //    # The default username for our AMI
-  //    user = var.ami_username
-  //    # Private key for connection
-  //    private_key = file(pathexpand(var.private_key))
-  //    # Type of connection
-  //    type = "ssh"
-  //  }
-
-  //  provisioner "file" {
-  //   source = "s3copy.sh"
-  //   destination = "${var.ami_user_home}/s3copy.sh"
-  // }
 }
 
 // generate inventory file
